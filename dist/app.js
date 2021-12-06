3567af522141801a98acacf7eeb894192e401604
JangGeonil <wkdrjsdlf2@gmail.com>
2021-12-06 19:15:16 +0900

    많은 것들을 직접 해봐야지 실력이 늘어요

--- dist/app.js
+++ dist/app.js
@@ -8,13 +8,13 @@ class App {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
-        image.attachTo(appRoot, "beforeend");
+        this.page.addChild(image);
         const video = new VideoComponent("Video", "https://www.youtube.com/embed/JS-Si5GO3iA");
-        video.attachTo(appRoot, "beforeend");
+        this.page.addChild(video);
         const note = new NoteComponent("Note Title", "Note Body");
-        note.attachTo(appRoot, "beforeend");
+        this.page.addChild(note);
         const todo = new TodoComponent("Todo Title", "??");
-        todo.attachTo(appRoot, "beforeend");
+        this.page.addChild(todo);
     }
 }
 new App(document.querySelector(".document"));
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+import { Component } from "./components/component.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
 
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
@@ -14,19 +15,19 @@ class App {
       "Image Title",
       "https://picsum.photos/600/300"
     );
-    image.attachTo(appRoot, "beforeend");
+    this.page.addChild(image);
 
     const video = new VideoComponent(
       "Video",
       "https://www.youtube.com/embed/JS-Si5GO3iA"
     );
-    video.attachTo(appRoot, "beforeend");
+    this.page.addChild(video);
 
     const note = new NoteComponent("Note Title", "Note Body");
-    note.attachTo(appRoot, "beforeend");
+    this.page.addChild(note);
 
     const todo = new TodoComponent("Todo Title", "??");
-    todo.attachTo(appRoot, "beforeend");
+    this.page.addChild(todo);
   }
 }
 
